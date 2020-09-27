735159801c75fa5125d0e1f0e06899d3695e998a
tabatalisboa <tabatalisboa@pm.me>
2020-09-27 00:12:43 -0300

    Add Actions

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,4 +1,25 @@
 class PodcastsController < ApplicationController
   def index
+    @podcasts = Podcast.all('created_at DESC')
   end
+
+  def new
+    @podcast = Podcast.new
+  end
+
+  def create
+    @podcast = Podcast.new(podcast_params)
+    if @podcast.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def podcast_params
+    params.require(:podcast).permit(:title, :description, :author)
+  end
+
 end
+
