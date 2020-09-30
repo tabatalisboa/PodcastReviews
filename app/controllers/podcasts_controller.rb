0a313fe83d4d7895769c392852270f8349e6a77a
tabatalisboa <tabatalisboa@pm.me>
2020-09-30 00:20:29 -0300

    Add edit,show,update actions and views

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,6 +1,12 @@
 class PodcastsController < ApplicationController
+  before_action :find_podcast, only: [:show, :edit, :update, :destroy]
+
   def index
     @podcasts = Podcast.all.order('created_at DESC')
+  end
+
+  def show
+    # @podcast = Podcast.find(params[:id])
   end
 
   def new
@@ -16,10 +22,29 @@ class PodcastsController < ApplicationController
       render :new
     end
   end
+  def edit
+  end
+
+  def update
+    if @podcast.update(podcast_params)
+      redirect_to podcast_path(@podcast)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @podcast.destroy
+    redirect_to root_path
+  end
 
   private
+
   def podcast_params
     params.require(:podcast).permit(:title, :description, :author)
   end
 
+  def find_podcast
+    @podcast = Podcast.find(params[:id])
+  end
 end
