6feca48efeefbbd4928244cfc5472789a9006a41
tabatalisboa <tabatalisboa@pm.me>
2020-10-10 17:25:40 -0300

    Add Associations to Models

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -2,4 +2,5 @@ class Podcast < ApplicationRecord
   has_one_attached :photo
   belongs_to :user
   belongs_to :category
+  has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_08_191847) do
+ActiveRecord::Schema.define(version: 2020_10_10_201415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2020_10_08_191847) do
     t.bigint "podcast_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
     t.index ["podcast_id"], name: "index_reviews_on_podcast_id"
   end
 
